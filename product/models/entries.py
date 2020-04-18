8f6211ec4df908f1dff0d3a6b646295fb0d434cb
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2020-04-18 18:10:17 +0900

    :sparkles:：機能追加：モデルファイルの実装
    本アプリケーションで使用するモデルファイルを作成した。

---
+++ product/models/entries.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from pynamodb.attributes import NumberAttribute, UnicodeAttribute, UTCDateTimeAttribute
+from pynamodb.models import Model
+
+
+class Entry(Model):
+    class Meta:
+        table_name = "serverless_blog_entries"
+        region = "ap-northeast-1"
+        aws_access_key_id = "AWS_ACCESS_KEY_ID"
+        asw_secret_access_key = "ASW_SECRET_ACCESS_KEY"
+        host = "http://localhost:8000"
+
+    id = NumberAttribute(hash_key=True, null=False)
+    title = UnicodeAttribute(null=True)
+    text = UnicodeAttribute(null=True)
+    created_at = UTCDateTimeAttribute(default=datetime.now)
+    modified_at = UTCDateTimeAttribute(default=datetime.now)
