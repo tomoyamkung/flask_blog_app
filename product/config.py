8cf1f2ffc27d002bebe2c48f0746964c55e8096e
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2020-04-19 14:49:35 +0900

    :bug:：バグ修正：スペルミスを修正
    "SECRET_KEY" のスペルに誤りがあったのを修正した。

--- product/config.py
+++ product/config.py
@@ -3,13 +3,13 @@ import os
 
 class DevelopmentConfig(object):
     DEBUG = True
-    SECLET_KEY = "secret key"
+    SECRET_KEY = "secret_access_key key"
     USERNAME = "hoge"
     PASSWORD = "password"
 
 
 class ProductionConfig(object):
     DEBUG = False
-    SECLET_KEY = os.environ.get("SERVERLESS_SECRET_KEY")
+    SECRET_KEY = os.environ.get("SERVERLESS_SECRET_KEY")
     USERNAME = "hoge"
     PASSWORD = os.environ.get("SERVERLESS_PASSWORD")
